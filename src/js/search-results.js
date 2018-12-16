8accafeab47c112011db3a8016437ca6ecf33e9e
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-12-16 23:10:42 +0100

    Fix switch case

--- src/js/search-results.js
+++ src/js/search-results.js
@@ -5,22 +5,27 @@ const generateHTML = text => `<li>${text}</li>`;
 export const showResults = (searchOption, results) => {
    let html;
 
-   if (searchOption === 'films') {
-      html = results.map(result =>
-         generateHTML(`<b>Title:</b> ${result.title}, <b>Director:</b> ${result.director}`)
-      );
-   } else if (searchOption === 'people') {
-      html = results.map(result =>
-         generateHTML(`<b>Name:</b> ${result.name}, <b>Height:</b> ${result.height}`)
-      );
-   } else if (searchOption === 'planets') {
-      html = results.map(result =>
-         generateHTML(`<b>Name:</b> ${result.name}, <b>Climate:</b> ${result.climate}`)
-      );
-   } else if (searchOption === 'starships') {
-      html = results.map(result =>
-         generateHTML(`<b>Name:</b> ${result.name}, <b>Model:</b> ${result.model}`)
-      );
+   switch (searchOption) {
+      case 'films':
+         html = results.map(result =>
+            generateHTML(`<b>Title:</b> ${result.title}, <b>Director:</b> ${result.director}`)
+         );
+         break;
+      case 'people':
+         html = results.map(result =>
+            generateHTML(`<b>Name:</b> ${result.name}, <b>Height:</b> ${result.height}`)
+         );
+         break;
+      case 'planets':
+         html = results.map(result =>
+            generateHTML(`<b>Name:</b> ${result.name}, <b>Climate:</b> ${result.climate}`)
+         );
+         break;
+      case 'starships':
+         html = results.map(result =>
+            generateHTML(`<b>Name:</b> ${result.name}, <b>Model:</b> ${result.model}`)
+         );
+         break;
    }
    searchResultsEl.innerHTML = html.join(' ');
 };
